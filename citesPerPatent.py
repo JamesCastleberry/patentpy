850ad247058cb501878133a7464a43040dcbe8ad
JamesCastleberry <46984285+JamesCastleberry@users.noreply.github.com>
2019-05-07 13:48:31 -0400

    Early stage program for finding the number of cited patents.

--- citesPerPatent.py
+++ citesPerPatent.py
@@ -1,10 +1,16 @@
-#newFile = open('newStart.py')
+#This program will print the total number of patents cited by a given patent.
+
+#Asks the user what file they want to use.
 newFile = input('What file you want to use: ')
+
+#A counter that will be used for the cited patents
 counter = None
+
+#A for loop that steps through the lines of the file and counts the number of cited patents. If a new patent tag is found, the previous
+#number of cited patents is printed and then the counter is reset.
 for line in newFile:
     if line.startswith('<patcit'):
         counter = counter + 1
-        #print(line)
     if line.startswith('<invention-title'):
         if counter != None:
             print('The total number of cited patents was ',counter,'\n')
